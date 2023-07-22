99fa732ca8c197014713313291af41b85f2603a8
Jose Luis Huaman Villar <jlhuamanv@gmail.com>
2023-07-22 12:20:12 -0500

    Proyecto para encriptar archivos con jasypt

---
+++ spring-boot-encrip-file/src/main/java/pe/org/jhsystem/demos/lib/spring/boot/encrip/file/SpringBootEncripFileApplication.java
@@ -0,0 +1,13 @@
+package pe.org.jhsystem.demos.lib.spring.boot.encrip.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEncripFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEncripFileApplication.class, args);
+	}
+
+}
